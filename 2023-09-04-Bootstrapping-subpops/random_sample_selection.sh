c86c6dd3221e7b945f23b5ce804bf0e9f2bca175
gabrielluishernandez <glhernandezg@unal.edu.co>
2023-09-04 11:59:19 +0100

    sampling_with_replacment

--- 2023-09-04-Bootstrapping-subpops/random_sample_selection.sh
+++ 2023-09-04-Bootstrapping-subpops/random_sample_selection.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Input file containing sample names, one per line
-input_file="sample_names.txt"
+input_file="input/All_4pops_samples.txt"
 
 # Create a directory to store the sets
-mkdir -p sets
+mkdir sets
 
 # Define the number of sets and the size of each set
 num_sets=100
@@ -13,21 +13,16 @@ set_size=34
 # Read the sample names into an array
 mapfile -t sample_names < "$input_file"
 
-# Shuffle the sample names randomly
-shuffled_names=($(shuf -e "${sample_names[@]}"))
-
 # Loop to create 100 sets
 for i in $(seq 1 "$num_sets"); do
-    # Calculate the start and end indices for each set
-    start=$((($i - 1) * $set_size))
-    end=$(($i * $set_size - 1))
-    
     # Create a new set file for this iteration
     set_file="sets/set_$i.txt"
     
-    # Extract and save the sample names for this set
-    for j in $(seq "$start" "$end"); do
-        echo "${shuffled_names[j]}" >> "$set_file"
+    # Randomly sample with replacement
+    for j in $(seq 1 "$set_size"); do
+        # Generate a random index within the range of sample_names array
+        rand_index=$((RANDOM % ${#sample_names[@]}))
+        echo "${sample_names[rand_index]}" >> "$set_file"
     done
 done
 
