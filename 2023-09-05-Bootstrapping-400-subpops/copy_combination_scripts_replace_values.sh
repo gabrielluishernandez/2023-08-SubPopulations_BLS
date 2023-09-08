1f6dff4d51415ef9855f5c2cdbe87b52d8585163
gabrielluishernandez <glhernandezg@unal.edu.co>
2023-09-08 13:23:24 +0100

    candidate selection R

--- 2023-09-05-Bootstrapping-400-subpops/copy_combination_scripts_replace_values.sh
+++ 2023-09-05-Bootstrapping-400-subpops/copy_combination_scripts_replace_values.sh
@@ -3,10 +3,10 @@
 # Loop from 1 to 100
 for i in {1..100}; do
   # Create the new filename by replacing "Set1" with "Set${i}"
-  new_filename=$(echo "combine_NCD_Ballermix_Set1.sh" | sed "s/Set1/Set${i}/")
+  new_filename=$(echo "get_the_candidates_Set1.R" | sed "s/Set1/Set${i}/")
 
   # Copy the original script to the new filename
-  cp combine_NCD_Ballermix_Set1.sh "$new_filename"
+  cp get_the_candidates_Set1.R "$new_filename"
 
   # Replace all occurrences of "set-1" with "set-${i}" in the new file
   sed -i "s/set-1/set-${i}/g" "$new_filename"
