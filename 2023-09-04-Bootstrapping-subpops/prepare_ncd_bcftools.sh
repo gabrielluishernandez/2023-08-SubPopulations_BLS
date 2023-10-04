0b8103baa1a789cee0e86c2974c10a9b5f28ab54
gabrielluishernandez <glhernandezg@unal.edu.co>
2023-10-04 15:38:28 +0100

    rming old bstap

--- 2023-09-04-Bootstrapping-subpops/prepare_ncd_bcftools.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-module load parallel
-module load bcftools
-
-parallel -j0 "bcftools query -f '%CHROM\t%POS\t%CHROM|%POS\t%REF\t%ALT\t%AF\n' vcf/{} > tmp_ncd/{}_inputNCD.txt" :::: list_vcf_dir
-
